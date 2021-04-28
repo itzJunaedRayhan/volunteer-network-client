a571477e69dde47b9c35ff6235254425ba35a29b
Junaed Rayhan <junaedrayhan@gmail.com>
2021-04-28 17:57:44 +0600

    Done

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Event = ({event}) => {
     return (
-        <div className="event col-md-3">
+        <div className="event col-md-4 col-sm-6">
             <img style={{height: '300px'}} src={event.imgURL} alt=""/>
             <h3>{event.name}</h3>
         </div>
